f218faf66466ceeaf05e8cf6cc34607f1e6a73a6
Benjamin Hutton <ben@benhutton.com.au>
2017-10-20 07:26:07 +0800

    Cleaned up tests

--- tests/Unit/RouteTest.php
+++ tests/Unit/RouteTest.php
@@ -3,17 +3,12 @@
 namespace Tests\Unit;
 
 use Tests\TestCase;
-use Laravel\Passport\HasApiTokens;
 use App\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class RouteTest extends TestCase
 {
     use DatabaseMigrations;
-    protected $client;
-    protected $fillable = [
-        'id', 'name', 'email', 'password',
-    ];
 
     public function testRoot()
     {
@@ -35,8 +30,6 @@ class RouteTest extends TestCase
     {
 
         $user = factory(User::class)->create([
-            'id' => 1,
-            'name' => 'yish',
             'role' => 'author'
         ]);
 
@@ -51,7 +44,6 @@ class RouteTest extends TestCase
     {
         $user = factory(User::class)->create();
 
-
         $this->actingAs($user)
             ->withSession(['users' => 'fred bloggs'])
             ->get('/new-post')
@@ -62,66 +54,14 @@ class RouteTest extends TestCase
     public function testNewPostGet()
     {
         $user = factory(User::class)->create([
-            'id' => 1,
-            'name' => 'yish',
             'role' => 'author'
         ]);
 
         $this->actingAs($user)
-            ->withSession(['users' => 'fred bloggs'])
+            ->withSession(['foo' => 'bar'])
             ->get('/new-post')
             ->assertSuccessful();
 
     }
 
-    protected function asLoginUser(User $user){
-        return $this->actingAs($user)
-            ->withSession(['applocalemodel' => 'ca']);
-    }
-
-//    public function testNewPostPost()
-//    {
-//        $user = $this->authenticateUser();
-//        $id = uniqid();
-//
-//        $response = $this->actingAs($user)
-//            ->withSession(['foo' => 'bar'])
-//            ->withHeaders([
-//            'X-Header' => 'Value',])->json('POST', 'new-post',
-//            [
-//                'id' => $id,
-//                'title' => 'My Random Test Book',
-//                'slug' => 'Slug',
-//                'body' => 'Body',
-//                'active' => 1,
-//                'created_at' => '0000-00-00 00:00:00',
-//                'updated_at' => '0000-00-00 00:00:00',
-//                'author_id' => '1233'
-//            ])->assertSuccessful();
-//
-////        $response
-////            ->assertStatus(200)
-////            ->assertJson([
-////                'created' => true,
-////            ]);
-//
-//
-//
-//
-//    }
-
-    /**
-     * @return ?
-     */
-    public function authenticateUser()
-    {
-        $password = '1qazwsx'; // Our password
-
-        $user = factory(User::class)->create([
-            'name' => 'fred',
-            'email' => 'taylor@laravel.com',
-            'password' => bcrypt($password) // Save the encrypted
-        ]);
-        return $user;
-    }
 }
