75321a3d62832c8f2a2f313315158055eacaccf6
Benjamin Hutton <ben@benhutton.com.au>
2017-10-27 19:07:14 +0800

    Removed login and register menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -25,23 +25,12 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://benhutton.com.au">BenHutton.com.au</a>
+          <a class="navbar-brand" href="/">BenHutton.com.au</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul class="nav navbar-nav">
-            <li>
-              <a href="{{ url('/') }}">Home</a>
-            </li>
-          </ul>
           <ul class="nav navbar-nav navbar-right">
-            @if (Auth::guest())
-            <li>
-              <a href="{{ url('/login') }}">Login</a>
-            </li>
-            <li>
-              <a href="{{ url('/register') }}">Register</a>
-            </li>
-            @else
+            @if (!Auth::guest())
+
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
               <ul class="dropdown-menu" role="menu">
