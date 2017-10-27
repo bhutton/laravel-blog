f45bf340db0bbab4ed2b0d2c4503962496773fc7
Benjamin Hutton <ben@benhutton.com.au>
2017-10-27 19:07:14 +0800

    Removed login and register menu

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Blog Demo | Find All Together</title>
+    <title>BenHutton.com.au</title>
     <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
     <!-- Fonts -->
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
@@ -25,7 +25,7 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="http://www.findalltogether.com">Find All Together</a>
+          <a class="navbar-brand" href="http://benhutton.com.au">BenHutton.com.au</a>
         </div>
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav">
