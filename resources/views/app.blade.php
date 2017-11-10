79d8d0c3d2f011c2ada8ab5073a9b14f26fd3dc5
Benjamin Hutton <ben@benhutton.com.au>
2017-11-10 14:23:43 +0800

    Added piwik code directly into header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -23,7 +23,7 @@
         (function() {
             var u="//piwik.altairhosting.com/";
             _paq.push(['setTrackerUrl', u+'piwik.php']);
-            _paq.push(['setSiteId', '7']);
+            _paq.push(['setSiteId', '2']);
             var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
             g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
         })();
