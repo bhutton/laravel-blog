2558fd7290a8a2596ead928f163ccb662881508d
Benjamin Hutton <ben@benhutton.com.au>
2017-11-10 10:53:15 +0800

    Added piwik javascript file to footer of all pages.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -96,5 +96,6 @@
     <!-- Scripts -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/piwik.js') }}"></script>
   </body>
 </html>
